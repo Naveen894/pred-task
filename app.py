d0a0d2f48ce1ffb5358b042621315b54b21cb57a
Naveen894 <52451595+Naveen894@users.noreply.github.com>
2020-02-18 17:31:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def predict1():
     return render_template('predict1.html',a=a,task=task)
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=8080)
+    #app.run(debug=True)
